a7914245ef54ae1bfb817607861b01d9d686f6c9
darklod <giuseppe.magazu@gmail.com>
2020-02-06 13:53:38 +0100

    featuresss

---
+++ +tests/compute_features.m
@@ -0,0 +1,44 @@
+close all;
+clear all;
+clc;
+
+folders = ["Ferrero Rocher/", "Ferrero Noir/", "Rigetto/", "Raffaello/"];
+output = ["mat/rocher-", "mat/noir-", "mat/rejection-", "mat/raffaello-"];
+
+% vedi se usare un solo mat file o più per ogni classe... ⤴⤴⤴⤴
+    
+for f=1:4  % 1:2:4 per confrontare rocher e rigetto
+    images = utils.getfiles("Data/TrainingSet/" + folders(f));
+    images = images(1:110);
+    N = numel(images);
+
+    F = zeros(N, 2); % features vector
+       
+    if ~exist(output(f) + "cedd.mat", 'file') 
+        cedd = zeros(N, 1);
+        hist = zeros(N, 1);
+        
+        for i=1:N
+            im = imread(images{i});
+            c = utils.compute_CEDD(im);
+            cedd(i) = norm(c);
+            % hist(i) = norm(imhist(im));
+        end
+
+        % Normalization
+        % nhist = (hist - min(hist)) ./ (max(hist) - min(hist));
+        ncedd = (cedd - min(cedd)) ./ (max(cedd) - min(cedd));
+        save(output(f) + "cedd.mat", "ncedd");
+    end
+    
+    load(output(f) + "cedd.mat", "ncedd");
+    
+    %F = [nhist, ncedd];
+    
+    legend('-DynamicLegend');
+    hold on;
+    
+    plot(ncedd, 'DisplayName', output(f) + "cedd");
+    %plot(nhist , 'DisplayName', folders(f) + 'hist');
+    
+end
