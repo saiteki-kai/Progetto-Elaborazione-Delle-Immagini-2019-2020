4ec06b4fd7e1795eb64810f19fcbc159c8c85d24
darklod <giuseppe.magazu@gmail.com>
2020-02-08 17:27:05 +0100

    kanashimi

---
+++ +utils/readlabels.m
@@ -0,0 +1,5 @@
+function [images, labels] = readlabels(path, folder)
+    rows = readmatrix(path, 'OutputType', 'string', 'NumHeaderLines', 1);
+    images = folder + rows(:, 1);
+    labels = rows(:, 2);
+end
